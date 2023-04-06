c9a0d489d0aaa25f97bf3a97efa118a47105a485
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-04-06 22:44:17 +0600

    Task 1
    Task 1 without styles

---
+++ main.js
@@ -0,0 +1,24 @@
+let container = document.getElementById('container')
+let button = document.getElementById('add-item-btn')
+button.addEventListener('click', function () {
+    let newElement = document.createElement('div');
+    newElement.id = 'new_element';
+    newElement.className = 'new_element';
+    let newText = document.createElement('span');
+    newText.id = 'new_text';
+    newText.className = 'new_text';
+    newText.innerText = 'Close it immediately';
+    let newButton = document.createElement('button');
+    newButton.id = 'new_button';
+    newButton.className = 'new_button';
+    newButton.innerText = 'Close';
+    newButton.addEventListener('click', () => {
+        newElement.remove()
+    })
+    container.appendChild(newElement);
+    newElement.appendChild(newText);
+    newElement.appendChild(newButton);
+    setTimeout(() => {
+        newElement.remove()
+    }, 5000)
+})
